48b9945438855b9077b4f77267a197fd454da315
George Paloukis <gpalouk@codigia.com>
2012-04-22 20:27:29 +0300

    Success!! All components are working ok for Android!!

---
+++ assets/www/app.js
@@ -0,0 +1,258 @@
+Ext.application({
+  name: 'Sencha',
+
+  requires: [
+             'Ext.MessageBox'
+             ],
+
+             views: [],
+
+             icon: {
+               57: 'resources/icons/Icon.png',
+               72: 'resources/icons/Icon~ipad.png',
+               114: 'resources/icons/Icon@2x.png',
+               144: 'resources/icons/Icon~ipad@2x.png'
+             },
+
+             phoneStartupScreen: 'resources/loading/Homescreen.jpg',
+             tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
+
+             launch: function() {
+               Ext.define('Twitter.view.SearchBar', {
+                 extend: 'Ext.Toolbar',
+                 xtype : 'searchbar',
+                 requires: ['Ext.field.Search'],
+
+                 config: {
+                   ui: 'searchbar',
+                   layout: 'vbox',
+                   cls: 'big',
+
+                   items: [
+                           {
+                             xtype: 'title',
+                             title: 'Twitter Search'
+                           },
+                           {
+                             xtype: 'searchfield',
+                             placeHolder: 'Search...'
+                           }
+                           ]
+                 }
+               });
+
+               // The whole app UI lives in this tab panel
+               Ext.Viewport.add({
+                 xtype: 'tabpanel',
+                 fullscreen: true,
+                 tabBarPosition: 'bottom',
+
+                 items: [
+                         // This is the home page, just some simple html
+                         {
+                           title: 'Home',
+                           iconCls: 'home',
+                           cls: 'home',
+                           scrollable: true,
+                           html: [
+                                  '<img height=260 src="http://staging.sencha.com/img/sencha.png" />',
+                                  '<h1>Welcome to Sencha Touch</h1>',
+                                  "<p>Building the Getting Started app</p>",
+                                  '<h2>Sencha Touch (2.0.0)</h2>'
+                                  ].join("")
+                         },
+
+                         // This is the recent blogs page. It uses a tree store
+                          // to load
+                         // its data from blog.json
+                         {
+                           xtype: 'nestedlist',
+                           title: 'Blog',
+                           iconCls: 'star',
+                           cls: 'blog',
+                           displayField: 'title',
+
+                           store: {
+                             type: 'tree',
+
+                             fields: ['title', 'link', 'author', 'contentSnippet', 'content', {
+                               name: 'leaf',
+                               defaultValue: true
+                             }],
+
+                             root: {
+                               leaf: false
+                             },
+
+                             proxy: {
+                               type: 'jsonp',
+                               url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                               reader: {
+                                 type: 'json',
+                                 rootProperty: 'responseData.feed.entries'
+                               }
+                             }
+                           },
+
+                           detailCard: {
+                             xtype: 'panel',
+                             scrollable: true,
+                             styleHtmlContent: true
+                           },
+
+                           listeners: {
+                             itemtap: function(nestedList, list, index, element, post) {
+                               this.getDetailCard().setHtml(post.get('content'));
+                             }
+                           }
+                         },
+
+                         // This is the contact page, which features a form and
+                          // a button.
+                         // The button submits the form
+                         {
+                           xtype: 'formpanel',
+                           title: 'Contact Us',
+                           iconCls: 'user',
+                           url: 'contact.php',
+                           layout: 'vbox',
+
+                           items: [
+                                   {
+                                     xtype: 'fieldset',
+                                     title: 'Contact Us',
+                                     instructions: 'Email address is optional',
+
+                                     items: [
+                                             {
+                                               xtype: 'textfield',
+                                               label: 'Name',
+                                               name: 'name'
+                                             },
+                                             {
+                                               xtype: 'emailfield',
+                                               label: 'Email',
+                                               name: 'email'
+                                             },
+                                             {
+                                               xtype: 'textareafield',
+                                               label: 'Message',
+                                               name: 'message',
+                                               height: 90
+                                             }
+                                             ]
+                                   },
+                                   {
+                                     xtype: 'button',
+                                     text: 'Send',
+                                     ui: 'confirm',
+
+                                     // The handler is called when the button
+                                      // is tapped
+                                     handler: function() {
+
+                                       // This looks up the items stack above,
+                                        // getting
+                                       // a reference to the first form it see
+                                       var form = this.up('formpanel');
+
+                                       // Sends an AJAX request with the form
+                                        // data to
+                                       // the url specified above
+                                        // (contact.php).
+                                       // The success callback is called if we
+                                        // get a
+                                       // non-error response from the server
+                                       form.submit({
+                                         success: function() {
+                                           // The callback function is run when
+                                            // the
+                                           // user taps the 'ok' button
+                                           Ext.Msg.alert('Thank You', 'Your message has been received', function() {
+                                             form.reset();
+                                           });
+                                         }
+                                       });
+                                     }
+                                   }
+                                   ]
+                         },
+                         // From "Views" tutorial topic
+                         {
+                           xtype: 'panel',
+                           title: 'Search',
+                           iconCls: 'search',
+                           padding: 30,
+                           html: 'This is my search bar!',
+
+                           /*
+                             * items: [ { xtype: 'searchbar', docked: 'top' } ]
+                             */
+                           items: [
+                                  {
+                                    xtype: 'button',
+                                    text: 'Facebook Login',
+                                    padding: 10,
+                                    margin: 10,
+                                    ui: 'action',
+                                  
+                                    // The handler is called when the button
+                                     // is tapped
+                                    handler: function() {
+                                      var thisBtn = this;
+                                      FB.login(
+                                          function(response) {
+                                          if (response.status == 'connected') {
+                                            FB.api('/me/friends',
+                                                { fields: 'id' },  
+                                                function(response) {
+                                                  if (response.error) {
+                                                    Ext.Msg.alert(JSON.stringify(response.error));
+                                                  } else {
+                                                    var numOfFriends = response.data.length;
+                                                    thisBtn.setBadgeText(numOfFriends+' friends on FB!');
+                                                  }
+                                                }
+                                            );
+                                            Ext.Msg.alert('Facebook Login!!');
+                                          } else {
+                                            Ext.Msg.alert('Facebook NOT Login :(');
+                                          }
+                                          },
+                                          { scope: "email" }
+                                          );                                      
+                                      }
+                                  },
+                                  {
+                                    xtype: 'button',
+                                    text: 'Facebook Logout',
+                                    padding: 10,
+                                    margin: 10,
+                                    ui: 'decline',
+                                  
+                                    // The handler is called when the button
+                                     // is tapped
+                                    handler: function() {
+                                      FB.logout(function(response) {
+                                        Ext.Msg.alert('Facebook Logged out');
+                                        });              
+                                      }
+                                  }
+                                  ]
+                         }
+                         ]
+               });
+             },
+
+             onUpdated: function() {
+               Ext.Msg.confirm(
+                   "Application Update",
+                   "This application has just successfully been updated to the latest version. Reload now?",
+                   function(buttonId) {
+                     if (buttonId === 'yes') {
+                       window.location.reload();
+                     }
+                   }
+               );
+             }
+});
